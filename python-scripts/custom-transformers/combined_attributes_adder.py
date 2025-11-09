01c66e30d883f6c5d2ad95539c53b517b87900e2
vathuglife <thuandt582@gmail.com>
2025-11-09 10:47:01 +0700

    dva: Added Feature Scaling and Transformation pipelines content

---
+++ python-scripts/custom-transformers/combined_attributes_adder.py
@@ -0,0 +1,20 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+#ix: index of the column in the dataset. Example:
+# rooms_ix = 3: Rooms column is at index 3, with index starts at 0.
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6 
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
